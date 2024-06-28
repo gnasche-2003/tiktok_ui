28485d8bfd21b0e1fce903e970326399727d3786
Pham Quoc Chuong <chuong.pqc2206@gmail.com>
2024-06-28 09:49:05 +0700

    Setups CSS/SASS & Router/Layout

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+// Pages
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Search from '~/pages/Search';
+import Upload from '~/pages/Upload';
+
+// Public Routes
+const publicRoutes = [
+  { path: '/', component: Home },
+  { path: '/following', component: Following },
+  { path: '/profile', component: Profile },
+  { path: '/search', component: Search, layout: null },
+  { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+// Private Routes
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
